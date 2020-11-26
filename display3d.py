8de3cdea67f2522dc709d0f813a73f72ea3f453b
Le Brun Victor <victor.lebrun@student-cs.fr>
2020-11-26 14:32:42 +0100

    bug fix

--- display3d.py
+++ display3d.py
@@ -3,23 +3,35 @@ from matplotlib import pyplot as plt
 from rcompute import *
 
 
-def rep3d(ax, vc, vr):
+def rep3d(ax, vc, vr, t=5):
     """ Créée dans ax un repère de centre vc et de rotation vr. """
-    v1, v2, v3 = rep_rot(float(vr[0]), float(vr[1]), float(vr[2]))
+    v1, v2, v3 = rep_rot(float(vr[0]), float(vr[1]), float(vr[2]), 5)
+
+    #print(v1, v2, v3)
 
     x, y, z = float(vc[0]), float(vc[1]), float(vc[2])
 
-    ax.quiver(x, y, z, v1[0], v1[1], v1[2])
-    ax.quiver(x, y, z, v2[0], v2[1], v2[2])
-    ax.quiver(x, y, z, v3[0], v3[1], v3[2])
+    #print(x, y, z)
+
+    ax.quiver(x, y, z, v1[0], v1[1], v1[2], color='r')
+    ax.quiver(x, y, z, v2[0], v2[1], v2[2], color='g')
+    ax.quiver(x, y, z, v3[0], v3[1], v3[2], color='b')
 
 
-def graph3d(tabdata):
+def graph3d(tabdata, tg=60):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
 
+    ax.set_xlim([-tg, tg])
+    ax.set_ylim([-tg, tg])
+    ax.set_zlim([-tg, tg])
+
+    ax.set_xlabel('axe x')
+    ax.set_ylabel('axe y')
+    ax.set_zlabel('axe z')
+
     for e in tabdata:
         if e[0] != 'id' and e[0] != '-1':
-            rep3d(ax, e[1:4], e[4:7])
+            rep3d(ax, e[1:4], e[4:7], tg/15)
 
     plt.show()
--- rcompute.py
+++ rcompute.py
@@ -4,27 +4,31 @@ import numpy as np
 def mat_rot(rx, ry, rz):
     """ Crée une matrice de rotation rx, ry, rz. """
 
+    rx *= np.pi
+    ry *= np.pi
+    rz *= np.pi
+
     Rmx = np.array([[1, 0, 0],
                     [0, np.cos(rx), -np.sin(rx)],
-                    [0, np.sin(rx), -np.sin(rx)]])
+                    [0, np.sin(rx), np.cos(rx)]])
 
     Rmy = np.array([[np.cos(ry), 0, np.sin(ry)],
                     [0, 1, 0],
                     [-np.sin(ry), 0, np.cos(ry)]])
 
     Rmz = np.array([[np.cos(rz), -np.sin(rz), 0],
-                    [np.sin(rz), -np.sin(rz), 0],
+                    [np.sin(rz), np.cos(rz), 0],
                     [0, 0, 1]])
 
     R = np.dot(Rmy, Rmz)
     return np.dot(Rmx, R)
 
 
-def rep_rot(rx, ry, rz):
+def rep_rot(rx, ry, rz, t=1):
     """ Renvoie trois vecteurs unitaires normalisés avec une rotation rx, ry, rz. """
-    v1 = np.array([1, 0, 0])
-    v2 = np.array([0, 1, 0])
-    v3 = np.array([0, 0, 1])
+    v1 = np.array([t, 0, 0])
+    v2 = np.array([0, t, 0])
+    v3 = np.array([0, 0, t])
 
     R = mat_rot(rx, ry, rz)
 
